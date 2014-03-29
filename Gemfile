5fac1ebed5b6c4263b1eb0595d44c87b51e6141f
Thomas Clavier <tom@tcweb.org>
2014-03-29 12:03:03 +0100

    first browser test OK

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :rubygems
+gem "sinatra"
+gem "haml"
+gem "data_mapper"
+gem "dm-sqlite-adapter"
+gem "sqlite3"
+
+group :test do
+  gem "shotgun"
+end
+
---
+++ classroomci.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+class Projet 
+  include DataMapper::Resource
+  property :id,      Serial
+  property :url,     Text, :required => true
+  property :student, Text
+end
+
+class ClassroomCI < Sinatra::Application
+  @flash
+
+  configure do
+    set :haml, :format => :html5
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/database.db")
+    DataMapper.finalize.auto_upgrade!
+  end
+
+  error do
+    'Sorry there was a nasty error - ' + env['sinatra.error'].name
+  end
+
+  before do
+    cache_control :public, :must_revalidate, :max_age => 60
+  end
+
+  get '/' do
+    haml :index
+  end
+end
+
